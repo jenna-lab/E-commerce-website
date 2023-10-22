63901722a60491eedf401291eae040df36372ccd
jennifer sammy <jensam209@gmail.com>
2023-10-22 19:51:08 +0300

    add files

--- script.js
+++ script.js
@@ -1,34 +1,80 @@
-// Function to fetch data from the API
-async function fetchData(url) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+var apiUrl = "https://fakestoreapi.com/products";
+function displaySingleProduct(product) {
+    var productContainer = document.getElementById("product-container");
+    if (!productContainer)
+        return;
+    productContainer.innerHTML = "";
+    var productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = "\n    <img src=\"".concat(product.image, "\" alt=\"").concat(product.title, "\" />\n    <h3>").concat(product.title, "</h3>\n    <p>Category: ").concat(product.category, "</p>\n    <p>Price: $").concat(product.price, "</p>\n    <p>Description: ").concat(product.description, "</p>\n    <p>Rating: ").concat(product.rating.rate, " (").concat(product.rating.count, " reviews)</p>\n  ");
+    productContainer.appendChild(productDiv);
 }
-
-// Function to update the navbar with fetched data
-async function updateNavbar() {
-  const apiUrl = "https://api.example.com/navbar"; // Replace with your API endpoint
-  const navbarData = await fetchData(apiUrl);
-  const storeName = document.getElementById("storeName");
-  const navList = document.getElementById("navList");
-
-  if (navbarData) {
-    storeName.textContent = navbarData.storeName;
-    navList.innerHTML = "";
-    navbarData.navItems.forEach((item) => {
-      const li = document.createElement("li");
-      const a = document.createElement("a");
-      a.href = item.link;
-      a.textContent = item.title;
-      li.appendChild(a);
-      navList.appendChild(li);
+function displayAllProducts(products) {
+    var productContainer = document.getElementById("product-container");
+    if (!productContainer)
+        return;
+    productContainer.innerHTML = "";
+    if (!products || products.length === 0) {
+        productContainer.innerHTML = "No products to display.";
+        return;
+    }
+    products.forEach(function (product) {
+        var productDiv = document.createElement("div");
+        productDiv.classList.add("product");
+        productDiv.innerHTML = "\n      <img src=\"".concat(product.image, "\" alt=\"").concat(product.title, "\" />\n      <h3>").concat(product.title, "</h3>\n      <p>Category: ").concat(product.category, "</p>\n      <p>Price: $").concat(product.price, "</p>\n    ");
+        productDiv.addEventListener("click", function () { return displaySingleProduct(product); });
+        productContainer.appendChild(productDiv);
     });
-  }
 }
-
-// Call the function to update the navbar
-updateNavbar();
+function viewMore(description, rating, count) {
+    var modal = document.createElement("div");
+    modal.classList.add("modal");
+    var modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content");
+    modalContent.innerHTML = "\n    <span class=\"close-button\" onclick=\"closeModal()\">&times;</span>\n    <p>Description: ".concat(description, "</p>\n    <p>Rating: ").concat(rating, " (").concat(count, " reviews)</p>\n  ");
+    modal.appendChild(modalContent);
+    document.body.appendChild(modal);
+}
+function closeModal() {
+    var modal = document.querySelector(".modal");
+    if (modal) {
+        modal.remove();
+    }
+}
+var categories = [];
+function extractCategories(products) {
+    var categories = [];
+    products.forEach(function (product) {
+        if (categories.indexOf(product.category) === -1) {
+            categories.push(product.category);
+        }
+    });
+    return categories;
+}
+function displayCategory(categories, selectedCategory) {
+    var navUl = document.getElementById("nav-ul");
+    if (navUl) {
+        var ul_1 = navUl.querySelector("ul");
+        if (ul_1) {
+            categories.forEach(function (category) {
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+                a.href = "javascript:void(0)";
+                a.onclick = function () { return displayCategory(categories, category); };
+                a.textContent = category;
+                li.appendChild(a);
+                ul_1.appendChild(li);
+            });
+        }
+    }
+}
+fetch(apiUrl)
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
+    var categories = extractCategories(data);
+    displayCategory(categories, ""); // Initially, no category is selected
+    displayAllProducts(data);
+})
+    .catch(function (error) {
+    console.error("Error fetching data:", error);
+});
---
+++ script.ts
@@ -0,0 +1,175 @@
+const apiUrl: string = "https://fakestoreapi.com/products";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: { rate: number; count: number };
+}
+
+// function displaySingleProduct(product: Product): void {
+//   const productContainer = document.getElementById("product-container");
+//   if (!productContainer) return;
+
+//   productContainer.innerHTML = "";
+//   const productDiv = document.createElement("div");
+//   productDiv.classList.add("product");
+
+//   productDiv.innerHTML = `
+//     <img src="${product.image}" alt="${product.title}" />
+//     <h3>${product.title}</h3>
+//     <p>Category: ${product.category}</p>
+//     <p>Price: $${product.price}</p>
+//     <p>Description: ${product.description}</p>
+//     <p>Rating: ${product.rating.rate} (${product.rating.count} reviews)</p>
+//   `;
+
+//   productContainer.appendChild(productDiv);
+// }
+function displaySingleProduct(product: Product): void {
+  const productContainer = document.getElementById("product-container");
+  if (!productContainer) return;
+
+  productContainer.innerHTML = "";
+
+  // Loading the product.html content
+  fetch("product.html")
+    .then((response) => response.text())
+    .then((html) => {
+      productContainer.innerHTML = html;
+
+      // Populating the product details
+      const productTitleElements = document.querySelectorAll("#productTitle");
+      productTitleElements.forEach((element) => {
+        element.innerText = product.title;
+         element.style.textDecoration = "none"; 
+      });
+
+      const productCategoryElement = document.getElementById("productCategory");
+      if (productCategoryElement) {
+        productCategoryElement.innerText = `Category: ${product.category}`;
+      }
+
+      const productPriceElements = document.querySelectorAll("#productPrice");
+      productPriceElements.forEach((element) => {
+        element.innerText = `$${product.price}`;
+      });
+
+      const productDescriptionElements = document.querySelectorAll(
+        "#productDescription"
+      );
+      productDescriptionElements.forEach((element) => {
+        element.innerText = product.description;
+      });
+
+      const productRatingElement = document.getElementById("productRating");
+      if (productRatingElement) {
+        productRatingElement.innerText = `${product.rating.rate} (${product.rating.count} reviews)`;
+      }
+
+      const productImageElements = document.querySelectorAll("#productImage");
+      productImageElements.forEach((element) => {
+        element.setAttribute("src", product.image);
+        element.setAttribute("alt", product.title);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching product.html:", error);
+    });
+}
+
+function displayAllProducts(products: Product[]): void {
+  const productContainer = document.getElementById("product-container");
+  if (!productContainer) return;
+
+  productContainer.innerHTML = "";
+  if (!products || products.length === 0) {
+    productContainer.innerHTML = "No products to display.";
+    return;
+  }
+
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+
+    productDiv.innerHTML = `
+      <img src="${product.image}" alt="${product.title}" />
+      <h3>${product.title}</h3>
+      <p>Category: ${product.category}</p>
+      <p>Price: $${product.price}</p>
+    `;
+
+    productDiv.addEventListener("click", () => displaySingleProduct(product));
+
+    productContainer.appendChild(productDiv);
+  });
+}
+
+
+function viewMore(description: string, rating: number, count: number): void {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("modal-content");
+
+  modalContent.innerHTML = `
+    <span class="close-button" onclick="closeModal()">&times;</span>
+    <p>Description: ${description}</p>
+    <p>Rating: ${rating} (${count} reviews)</p>
+  `;
+
+  modal.appendChild(modalContent);
+  document.body.appendChild(modal);
+}
+
+function closeModal() {
+  const modal = document.querySelector(".modal");
+  if (modal) {
+    modal.remove();
+  }
+}
+let categories: string[] = [];
+
+
+function extractCategories(products: Product[]): string[] {
+  const categories: string[] = [];
+  products.forEach((product) => {
+    if (categories.indexOf(product.category) === -1) {
+      categories.push(product.category);
+    }
+  });
+  return categories;
+}
+
+function displayCategory(categories: string[], selectedCategory: string): void {
+  const navUl = document.getElementById("nav-ul");
+  if (navUl) {
+    const ul = navUl.querySelector("ul");
+    if (ul) {
+      categories.forEach((category) => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = "javascript:void(0)";
+        a.onclick = () => displayCategory(categories, category);
+        a.textContent = category;
+        li.appendChild(a);
+        ul.appendChild(li);
+      });
+    }
+  }
+}
+
+fetch(apiUrl)
+  .then((response: Response) => response.json())
+  .then((data: Product[]) => {
+    const categories = extractCategories(data);
+    displayCategory(categories, ""); // Initially, no category is selected
+    displayAllProducts(data);
+  })
+  .catch((error: any) => {
+    console.error("Error fetching data:", error);
+  });
